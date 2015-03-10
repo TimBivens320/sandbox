3217d714c06971eee0188a57fa3a7b4bcd74bf53
bivenst <tbivens320@gmail.com>
2015-03-10 09:58:24 -0400

    temp enum

---
+++ TempClass.java
@@ -0,0 +1,23 @@
+package AgilitySolutions.DataModel.Enums;
+
+public enum AddressType {
+    Mailing(1, "Mailing"),
+    Shipping(2, "Shipping"),
+    Billing(2, "Billing");
+
+    private final int id;
+    private final String description;
+
+    private AddressType(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
